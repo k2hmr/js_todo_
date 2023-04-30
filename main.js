54e9d4017cd4c5e731032d17c54dc338cddbe2a1
kay <72641683+k2hmr@users.noreply.github.com>
2023-04-30 13:53:22 +0900

    タスクが入力されてない場合は追加しないようにする

--- main.js
+++ main.js
@@ -121,6 +121,6 @@ deleteCancelButton.addEventListener("click", (e) => {
 taskSubmit.addEventListener("click", (e) => {
   e.preventDefault();
   const task = taskValue.value;
-  addTasks(task);
+  task && addTasks(task);
   taskValue.value = "";
 });
